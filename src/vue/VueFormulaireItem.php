5f988e422bdcba45be07a5427fea7e4635671aeb
AntonyNigro <antony.nigro4@etu.univ-lorraine.fr>
2022-01-04 11:48:48 +0100

    debut de la vueFormulaire item

--- src/vue/VueFormulaireItem.php
+++ src/vue/VueFormulaireItem.php
@@ -1,1 +1,26 @@
 <?php
+namespace wishlist\vue;
+
+class VueFormulaireItem{
+
+    private array $array;
+
+    function __construct(array $a){
+        $this->array = $a;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
