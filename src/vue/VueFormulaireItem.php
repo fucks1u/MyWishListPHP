be7653b999e0bd20f53e62a7f56fb8156fdce876
AntonyNigro <antony.nigro4@etu.univ-lorraine.fr>
2022-01-05 15:54:48 +0100

    ajout du css pour la vue formulaire item

--- src/vue/VueFormulaireItem.php
+++ src/vue/VueFormulaireItem.php
@@ -11,26 +11,26 @@ class VueFormulaireItem{
  <head>
       <title>Création Item</title>
       <meta charset="utf-8">
-      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/styleItem.css">
     </head>
     <body>
+    <div id="container">
  <form action="/creationItem" method="post">
-    <div>
-        <label for="name">Nom :</label>
+   <h1>Création d'un Item</h1>
+
+        <label for="name"><b>Nom de l'Item :</b></label>
         <input type="text" id="nomItem" name="item_name">
-    </div>
-    <div>
-        <label for="prix">Prix :</label>
+
+        <label for="prix"><b>Prix :</b></label>
         <input type="text" id="prixItem" name="item_price">
-    </div>
-    <div>
-        <label for="desc">Description :</label>
+
+        <label for="desc"><b>Description :</b></label>
         <textarea id="descItem" name="item_description"></textarea>
-    </div>
-    <div class="button">
-        <button type="submit">Valider</button>
-    </div>
+        
+        <input type="submit" id='submit' value="Créer l'item">
+    
 </form>
+</div>
 </body><html>
 END;
 
