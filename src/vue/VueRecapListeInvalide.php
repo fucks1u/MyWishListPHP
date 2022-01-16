16299a39f6a638babcfb73dd7a7147358f8e0ee4
AntonyNigro <antony.nigro4@etu.univ-lorraine.fr>
2022-01-16 17:19:49 +0100

    ajout et modification de la vue recap invalide + css

--- src/vue/VueRecapListeInvalide.php
+++ src/vue/VueRecapListeInvalide.php
@@ -6,19 +6,25 @@ class VueRecapListeInvalide{
 
 
     public function render() : mixed{
+           
         $html = <<<END
 <!DOCTYPE html> 
 <html>
     <head>
         <title>Page de traitement</title>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="../css/styleRecapListeInvalide.css">
     </head>
     <body>
-        <h2>Dans le formulaire précédent, vous avez fourni des informations invalides :</h2>
+    
+        <h1>Dans le formulaire précédent, vous avez fourni des informations invalides :</h1>
+        <div class="fond"> 
         
-        <p>Nom : <b>Caramel</b>, doit etre du type texte => "Caramel"</p>
-        <p>Prix : <b>15</b></p>
-        <p>Description : <b>Description pour l'item Caramel</b></p>
+        <p><b>Nom:</b> Caramel, doit etre du type <span class="check">texte</span>=> <span class="solution">"Caramel"</span></p>
+        <p><b>Date:</b> doit etre remplie avec des <span class="check">nombres</span> comme par exemple <span class="solution"> 25/12/2022</span></p>
+        <p><b>Description:</b><br></br>doit comporter du <span class="check">texte</span> pour décrire la liste comme par exemple :<br></br>  <span class="solution">
+        Cette liste regroupe les Items de mes rêves! </span></p>
+        </div>
     </body>
 </html>
 END;
