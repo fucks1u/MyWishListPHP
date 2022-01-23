6ba73d3768af366ff490fa32761fd8a1f6e72626
FUCKS Ariirau <ariirau.fucks7@etu.univ-lorraine.fr>
2022-01-23 18:47:38 +0100

    Ajout commentaire

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -29,33 +29,13 @@ class Controller{
         $this->container = $c;
  }
 
-
-    public function getItem($rq, $rs, $args) : Response{
-        $base = $rq->getUri()->getBasePath();
-        $id = $args['id'];
-        $item = Item::where('id','=',$args['id'])->first();
-        $res = Reservation::where('id_item','=',$item->id)->first();
-        $v = new \wishlist\vue\VueItem([$item]);
-        if($res == null){
-            $v->setReservation(false);
-        } else {
-            $v->setReservation(true);
-        }
-        $rs->getBody()->write($v->render($base));
-        return $rs;
-    }
+ /******************************
+  *   Partie Liste
+  *******************************/
 
     /*
-     * -------------------------------------------------
-     * - Methodes pour la gestion et creation de Liste -
-     * -------------------------------------------------
+     * recuperer une liste avec les donnees fournis par un formulaire
      */
-    public function getFormListId($rq, $rs, $args){
-        $v = new VueRechercheListe();
-        $rs->getBody()->write($v->render());
-        return $rs;
-    }
-
     public function getList($rq, $rs, $args){
         $data = $rq->getParsedBody();
         $base = $rq->getUri()->getBasePath();
@@ -72,18 +52,18 @@ class Controller{
         }
     }
 
-
-    public function reservationItem($rq, $rs, $args){
-        $iditem = $args['id'];
-        $item = Item::where('id','=',$iditem)->get();
-        $i = $item[0];
-        $idlist = $i->liste_id;
-        Reservation::insert(['id_list'=>$idlist,'id_item'=>$iditem,'token'=>$_COOKIE["participant_cookie"]]);
-        return $rs->withRedirect($this->container->router->pathFor('item_recap', ['id' => $iditem]));
+    /*
+     * recuperer un formulaire pour la recherche d'une liste grace a son id
+     */
+    public function getFormListId($rq, $rs, $args){
+        $v = new VueRechercheListe();
+        $rs->getBody()->write($v->render());
+        return $rs;
     }
 
-
-
+    /*
+     * Methode pour recuperer une liste grace a son id
+     */
     public function getListId($rq, $rs, $args){
         $id = $args['id'];
         $list = Liste::where('no', '=',$id)->get();
@@ -94,6 +74,9 @@ class Controller{
         return $rs;
     }
 
+    /*
+     * Methode pour retourner toutes les listes d'un participant grace a son token
+     */
     public function getMyList($rq, $rs, $args){
         $token = $_COOKIE["participant_cookie"];
         if($token == null){
@@ -109,12 +92,18 @@ class Controller{
         return $rs;
     }
 
+    /*
+     * Methode qui retourne un formulaire pour la creation d'une liste
+     */
     public function getFormList($rq, $rs, $args){
         $v = new VueFormulaireListe();
         $rs->getBody()->write($v->render());
         return $rs;
     }
 
+    /*
+     * Methode qui creer une liste grace aux donnees fournis dans le formulaire
+     */
     public function createList($rq,$rs, $args){
         $this->verifCookie();
         $data = $rq->getParsedBody();
@@ -140,51 +129,59 @@ class Controller{
         return $rs;
     }
 
+    /*
+     * Vue recapitulative des donnees du formulaire pour la creation d'une liste
+     */
     public function recap($rq, $rs, $args){
-            //création de la vue racapitulative
-            $list = Liste::where('no', '=', $args['list'])->first();
-            $data = array(
-                'list_title' => $list->titre,
-                'list_date' => $list->expiration,
-                'list_description' => $list->description,
-            );
-            $v = new VueRecapListe($data, $id = $list['no']);
-            $rs->getBody()->write($v->render());
-    }
-
-
-    public function getMessageList($rq, $rs, $args){
-        $v = new \wishlist\vue\VueAjoutMessage($args);
+        //création de la vue racapitulative
+        $list = Liste::where('no', '=', $args['list'])->first();
+        $data = array(
+            'list_title' => $list->titre,
+            'list_date' => $list->expiration,
+            'list_description' => $list->description,
+        );
+        $v = new VueRecapListe($data, $id = $list['no']);
         $rs->getBody()->write($v->render());
-        return $rs;
     }
 
-    public function getMessages($rq, $rs, $args){
+
+
+    /******************************
+     *   Partie Item
+     *******************************/
+
+    /*
+     * Methode permettant de recuperer un item et de l'afficher
+     */
+    public function getItem($rq, $rs, $args) : Response{
+        $base = $rq->getUri()->getBasePath();
         $id = $args['id'];
-        $mess = Message::where('id_liste', '=',$id)->get();
-        $v = new \wishlist\vue\VueMessages([$mess]);
-        $rs->getBody()->write($v->render());
-        return $rs;
-    }
-
-
-    public function getMessageRecap($rq, $rs, $args){
-        $data = $rq->getParsedBody();
-        $token = $_COOKIE['participant_cookie'];
-        try{
-            Message::insert(['id_liste'=>$args['id'],'message'=>$data['message'],'token'=>$token]);
-        } catch(PDOException $e){
-            throw new DBException('Ajout impossible : ' .$e->getMessage());
+        $item = Item::where('id','=',$args['id'])->first();
+        $res = Reservation::where('id_item','=',$item->id)->first();
+        $v = new \wishlist\vue\VueItem([$item]);
+        if($res == null){
+            $v->setReservation(false);
+        } else {
+            $v->setReservation(true);
         }
-        $v = new \wishlist\vue\VueAjoutMessage($args);
-        $rs->getBody()->write($v->renderRecap());
+        $rs->getBody()->write($v->render($base));
         return $rs;
     }
 
     /*
-     * ------------------------------------------------------------
-     * - Methode permettant d'afficher visuellement le formulaire -
-     * ------------------------------------------------------------
+     * Methode permettant de reserver un item dans la bdd
+     */
+    public function reservationItem($rq, $rs, $args){
+        $iditem = $args['id'];
+        $item = Item::where('id','=',$iditem)->get();
+        $i = $item[0];
+        $idlist = $i->liste_id;
+        Reservation::insert(['id_list'=>$idlist,'id_item'=>$iditem,'token'=>$_COOKIE["participant_cookie"]]);
+        return $rs->withRedirect($this->container->router->pathFor('item_recap', ['id' => $iditem]));
+    }
+
+    /*
+     * Methode permettant de retourner un formulaire pour l'ajout d'un item dans une liste
      */
     public function getFormItem($rq, $rs, $args){
         $v = new VueFormulaireItem($args['id']);
@@ -193,7 +190,7 @@ class Controller{
     }
 
     /*
-     * Methode permettant de recuperer les données présentes dans le formulaire
+    * Methode permettant de recuperer les données présentes dans le formulaire
      * et egalement l'afficher sur une page annexe
      */
     public function createItem($rq, $rs, $args){
@@ -218,11 +215,14 @@ class Controller{
             } catch(PDOException $e){
                 throw new DBException('Ajout impossible : ' .$e->getMessage());
             }
-             return $rs->withRedirect($this->container->router->pathFor('item_recap', ['item' => $item->id]));
+            return $rs->withRedirect($this->container->router->pathFor('item_recap', ['item' => $item->id]));
         }
         return $rs;
     }
 
+    /*
+     * Vuez recapitulative apres la creation d'un item
+     */
     public function recapItem($rq, $rs, $args){
         //création de la vue racapitulative
         $item = Item::where('id', '=', $args['item'])->first();
@@ -231,19 +231,60 @@ class Controller{
             'item_description'=>$item->descr,
             'item_price'=>$item->tarif,
         );
-            $v = new VueRecapItem($data);
-            $rs->getBody()->write($v->render());
+        $v = new VueRecapItem($data);
+        $rs->getBody()->write($v->render());
 
-    } 
+    }
 
+
+    /******************************
+     *   Partie Message
+     *******************************/
+
+    /*
+     * Methode permettant de retourner un formulaire pour l'ajout d'un message sur une liste donnee
+     */
+    public function getMessageList($rq, $rs, $args){
+        $v = new \wishlist\vue\VueAjoutMessage($args);
+        $rs->getBody()->write($v->render());
+        return $rs;
+    }
+
+    /*
+     * Methode retournant une vue avec tout les messages de la liste
+     */
+    public function getMessages($rq, $rs, $args){
+        $id = $args['id'];
+        $mess = Message::where('id_liste', '=',$id)->get();
+        $v = new \wishlist\vue\VueMessages([$mess]);
+        $rs->getBody()->write($v->render());
+        return $rs;
+    }
+
+    /*
+     * Vue de confirmation de l'ajout d'un message en bdd
+     */
+    public function getMessageRecap($rq, $rs, $args){
+        $data = $rq->getParsedBody();
+        $token = $_COOKIE['participant_cookie'];
+        try{
+            Message::insert(['id_liste'=>$args['id'],'message'=>$data['message'],'token'=>$token]);
+        } catch(PDOException $e){
+            throw new DBException('Ajout impossible : ' .$e->getMessage());
+        }
+        $v = new \wishlist\vue\VueAjoutMessage($args);
+        $rs->getBody()->write($v->renderRecap());
+        return $rs;
+    }
+
+
+    /*
+     * Methode permettant de savoir si le participant a deja un cookie ou non
+     */
     public function verifCookie(){
         if($_COOKIE["participant_cookie"] == null){
             $token = base_convert(hash('sha256', time() . mt_rand()), 16, 36);
             setcookie("participant_cookie",$token,time()+60*60*24*30);
         }
     }
-
-
-
-
 }
